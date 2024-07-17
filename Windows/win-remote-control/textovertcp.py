2194139334090ee6cb2af8915f7e2f0c8e26e6ac
Alex Mi. <43502191+mitzsch@users.noreply.github.com>
2024-07-17 18:27:01 +0200

    Add python tcp text send script
    Use: python textovertcp.py IP/Hostname Port keyinput
    
    Works with win-remote-control

---
+++ Windows/win-remote-control/textovertcp.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+data = " ".join(sys.argv[3:])
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes(data + "\n", "utf-8"))
+
+    # Receive data from the server and shut down
+    #received = str(sock.recv(1024), "utf-8")
+
+print("Sent:     {}".format(data))
+#print("Received: {}".format(received))
