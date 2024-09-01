df8c7df3ff559a592c9be19229f4af47a48ffe20
Alex Mitzsch <43502191+mitzsch@users.noreply.github.com>
2024-09-01 10:04:50 +0200

    mpv audio fix script
    This adds several different scripts. They all disable audio for a short time (0.4s - changeable) and reenable it. This is needed on some AVRs so they do not get overflown with audio data. With a short break, the audio gets streamed freshly again.
    
    There are three types:
    reinitonseek => sets the audio track to 0 and back again
    reinitaoonseek => checks current ao and sets it to null and back again (errors out when used too aggressively)
    reinitaoonseekV2 => desired ao is hardcoded (wasapi), sets to null and back again (not erroring out)

---
+++ Windows/mpv/scripts/reinitaoonseekV2.lua
@@ -0,0 +1,26 @@
+require "mp.msg"
+require "mp.options"
+
+local options = {
+	enable = true,          -- enable or disable the functionality
+}
+
+read_options(options)
+
+-- just load forced subs
+function control_audio()
+	if options.enable then
+               --disable the sound for a short while ... then re-enable
+
+		local out = "wasapi"
+		mp.set_property("ao","null")
+		
+		local time = mp.get_time()
+		while time + 0.4 > mp.get_time() and mp.get_property_bool("eof-reached") == false 
+		do end
+		
+		mp.set_property("ao",out)			
+	end
+end
+
+mp.register_event("seek", control_audio)
